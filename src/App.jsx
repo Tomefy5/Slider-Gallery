45c8b24f23571c8433812cb215a5caabb8a29bb5
tomefy andry tsiresy <tomefyandrytsiresy@gmail.com>
2024-12-12 23:53:01 +0300

    Automatic slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ButtonSlider from "./components/ButtonSlider";
 import Slider from "./components/Slider";
@@ -7,31 +7,49 @@ import ListImagesSlider from "./data/list-image-slider.json"
 
 function App() {
 
-  const [sliderCurrentIndex, setSliderIndex] = useState(1)
+  const [sliderCurrentIndex, setSliderIndex] = useState(0)
+  const [sliderAutoCurrentIndex, setSliderAutoIndex] = useState(0)
 
-  const slideHandler = (btnValue) => {
-    let newValue;
-    if(sliderCurrentIndex + btnValue > ListImagesSlider.length) {
-      newValue = 1
-    } else if (sliderCurrentIndex + btnValue < 1) {
-      newValue = ListImagesSlider.length
-    } else {
-      newValue = sliderCurrentIndex + btnValue
-    }
-
-    setSliderIndex(newValue)
+  const slideHandler = (direction) => {
+    setSliderIndex((prevIndex) => (
+      (prevIndex + direction + ListImagesSlider.length) % ListImagesSlider.length
+    ))
   }
 
+  const slideAutoHandler = (direction) => {
+    setSliderAutoIndex((prevIndex) => (
+      (prevIndex + direction + ListImagesSlider.length) % ListImagesSlider.length
+    ))
+  }
+
+  useEffect(() => {
+    const autoSlide = setInterval(() => {
+      slideAutoHandler(1)
+    },3000)
+
+    return () => clearInterval(autoSlide)
+  },[])
+
   return (
-    <div className="absolute bg-blue-950 w-full h-full">
+    <div className="absolute bg-blue-950 w-full h-auto pb-10 min-h-[100vh]">
       <h1 className="text-blue-300 text-xl md:text-2xl lg:text-4xl font-medium text-center mt-10">
-        Manual And Automatic Slider
+        Manual Slider
       </h1>
-      <h2 className="text-blue-100 font-bold text-md md:text-lg lg:text-xl text-center m-8">{`${sliderCurrentIndex} / ${ListImagesSlider.length}`}</h2>
+      <h2 className="text-blue-100 font-bold text-md md:text-lg lg:text-xl text-center m-8">{`${sliderCurrentIndex + 1} / ${ListImagesSlider.length}`}</h2>
       <div className="flex justify-center items-center">
         <ButtonSlider Icon={ChevronLeft} slideHandler={() => slideHandler(-1)} />
-        <Slider urlCurrentImage={ListImagesSlider[sliderCurrentIndex - 1].url} />
+        <Slider urlCurrentImage={ListImagesSlider[sliderCurrentIndex].url} />
         <ButtonSlider Icon={ChevronRight} slideHandler={() => slideHandler(1)}/>
+      </div>
+
+      <h1 className="text-blue-300 text-xl md:text-2xl lg:text-4xl font-medium text-center mt-10">
+        Automatic Slider
+      </h1>
+      <h2 className="text-blue-100 font-bold text-md md:text-lg lg:text-xl text-center m-8">{`${sliderAutoCurrentIndex + 1} / ${ListImagesSlider.length}`}</h2>
+      <div className="flex justify-center items-center mt-6">
+        <ButtonSlider Icon={ChevronLeft} slideHandler={() => slideAutoHandler(-1)} />
+        <Slider urlCurrentImage={ListImagesSlider[sliderAutoCurrentIndex].url} />
+        <ButtonSlider Icon={ChevronRight} slideHandler={() => slideAutoHandler(1)}/>
       </div>
     </div>
   );
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,7 @@
 export default function Slider({urlCurrentImage}) {
   return (
-    <div className='rounded overflow-hidden w-[280px] md:w-[400px] lg:w-[540px] h-auto relative bg-blue-800 mx-4 md:mx-8 lg:mx-12'>
-      <img className='object-cover aspect-auto' src={urlCurrentImage} alt="animal in snow" />
+    <div className='rounded overflow-hidden w-[280px] md:w-[400px] lg:w-[540px] h- aspect-[3/2] relative bg-blue-800 mx-4 md:mx-8 lg:mx-12'>
+      <img className='object-cover h-full' src={urlCurrentImage} alt="animal in snow" />
     </div>
   )
 }
